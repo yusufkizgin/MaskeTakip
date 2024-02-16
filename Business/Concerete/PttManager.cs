f70db89b4c3acc4cf1414440d2246496997efedc
Yusuf Kızgın <117774196+yusufkizgin@users.noreply.github.com>
2024-02-16 16:49:20 +0300

    Add project files.

---
+++ Business/Concerete/PttManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concerete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _service;
+
+        public PttManager(IApplicantService service)
+        {
+            _service = service;
+        }
+
+        public void GiveMask(Person person)
+        {
+            if (_service.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " İçin Maske Verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " İçin Maske Verilemedi!");
+            }
+        }
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concerete;
+using Entities.Concrete;
+
+
+List<string> sehirler = new List<string> { "Ankara", "İstanbul", "İzmir"};
+sehirler.Add("Adana");
+foreach (string s in sehirler)
+{
+    Console.WriteLine(s);
+}
+
+PttManager pttManager = new PttManager(new PersonManager());
+Person person1 = new Person();
+person1.FirstName = "Yusuf";
+person1.LastName = "Kızgın";
+person1.NationalIdentity = 13565461811;
+person1.DateOfBirthYear = 2004;
+pttManager.GiveMask(person1);
